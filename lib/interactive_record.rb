c9f76d404df4758240835eacc7c149ad2ce3b10d
Brennan Heisler <learn@heislercreative.com>
2018-03-11 20:24:49 +0000

    automatically backed up by learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -53,8 +53,8 @@ class InteractiveRecord
     DB[:conn].execute(sql)
   end
 
-  def self.find_by(attribute)
-    
+  def self.find_by(attribute_hash)
+    sql = "SELECT * FROM #{self.table_name} WHERE #{}"
   end
 
 end
